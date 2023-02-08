4bda6989a029a6647db5e4cfdedc25af8f514b2e
Devs Java <123784609+soudevjava@users.noreply.github.com>
2023-02-08 10:47:58 -0300

    projeto criado com estrutura de pacotes

---
+++ src/test/java/br/com/projetofaculdade/academico/AcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetofaculdade.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
